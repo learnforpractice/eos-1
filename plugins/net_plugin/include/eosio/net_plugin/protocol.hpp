54e2010ae9a257752ab0f7309977827655d2a96a
learnforpractice <learnforpractice@gmail.com>
2020-04-04 22:58:29 +0800

    Fix typo

--- plugins/net_plugin/include/eosio/net_plugin/protocol.hpp
+++ plugins/net_plugin/include/eosio/net_plugin/protocol.hpp
@@ -55,7 +55,7 @@ namespace eosio {
     validation, ///< the peer sent a block that failed validation
     benign_other, ///< reasons such as a timeout. not fatal but warrant resetting
     fatal_other, ///< a catch-all for errors we don't have discriminated
-    authentication ///< peer failed authenicatio
+    authentication ///< peer failed authentication
   };
 
   constexpr auto reason_str( go_away_reason rsn ) {
